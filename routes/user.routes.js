5de4a2756f8c837752fdf43f78a9c6ca867ec408
maxbrandt96 <max.brandtk@gmail.com>
2023-03-06 11:59:17 -0600

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-// TODO: Import user controller to handle requests
 const userController = require("../routes/user.controller");
 
+router.get("/", userController.getAllUsers);
 router.get("/:id", userController.getUserById);
+router.post("/", userController.createUser);
+router.put("/:id", userController.updateUserById);
+router.delete("/:id", userController.deleteUserById);
 
 module.exports = router;
