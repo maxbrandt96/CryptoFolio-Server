665abd664adbdc03c3cc33ef5311ec38d0a3cfb3
maxbrandt96 <max.brandtk@gmail.com>
2023-03-09 19:09:26 -0600

    Casi terminado/ faltan estilos

--- routes/portfolio.controller.js
+++ routes/portfolio.controller.js
@@ -53,3 +53,90 @@ exports.savePortfolio = async (req, res) => {
   }
 };
 
+
+exports.deleteCrypto = async (req, res) => {
+  const { userId, coinId } = req.params;
+
+  try {
+    const portfolio = await Portfolio.findOne({ userId });
+    if (!portfolio) {
+      return res.status(404).json({ message: "Portfolio not found" });
+    }
+
+    const updatedCryptos = { ...portfolio.cryptos };
+    delete updatedCryptos[coinId];
+
+    portfolio.cryptos = updatedCryptos;
+    await portfolio.save();
+
+    res.json({ message: "Crypto deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+exports.updateCrypto = async (req, res) => {
+  const { userId, coinId } = req.params;
+  const newAmount = req.body.amount;
+
+  try {
+    const portfolio = await Portfolio.findOne({ userId });
+    if (!portfolio) {
+      return res.status(404).json({ message: "Portfolio not found" });
+    }
+
+    const updatedCryptos = { ...portfolio.cryptos };
+    updatedCryptos[coinId] = newAmount;
+
+    portfolio.cryptos = updatedCryptos;
+    await portfolio.save();
+
+    res.json({ message: "Crypto updated successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+exports.editCoin = async function(req, res) {
+  try {
+    const { userId, coinId } = req.params;
+    const { quantity } = req.body;
+
+    const portfolio = await Portfolio.findOneAndUpdate(
+      { userId },
+      { $set: { [`cryptos.${coinId}`]: parseInt(quantity, 10) } },
+      { new: true }
+    );
+
+    if (!portfolio) {
+      return res.status(404).json({ message: "Portfolio not found" });
+    }
+
+    res.status(200).json({ message: "Coin quantity updated successfully" });
+  } catch (error) {
+    console.error("Error editing coin:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};exports.editCoin = async function(req, res) {
+  try {
+    const { userId, coinId } = req.params;
+    const { quantity } = req.body;
+
+    const portfolio = await Portfolio.findOneAndUpdate(
+      { userId },
+      { $set: { [`cryptos.${coinId}`]: parseInt(quantity, 10) } },
+      { new: true }
+    );
+
+    if (!portfolio) {
+      return res.status(404).json({ message: "Portfolio not found" });
+    }
+
+    res.status(200).json({ message: "Coin quantity updated successfully" });
+  } catch (error) {
+    console.error("Error editing coin:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
--- routes/portfolio.routes.js
+++ routes/portfolio.routes.js
@@ -9,6 +9,11 @@ router.get("/:userId/portfolio", portfolioController.getPortfolio);
 // Save portfolio for a user
 router.post("/:userId/portfolio", portfolioController.savePortfolio);
 
+// Delete a specific crypto from a user's portfolio
+router.delete("/:userId/portfolio/:coinId", portfolioController.deleteCrypto);
+
+// Update a specific crypto in a user's portfolio
+router.put("/:userId/portfolio/:coinId", portfolioController.editCoin);
 
 
 module.exports = router;
